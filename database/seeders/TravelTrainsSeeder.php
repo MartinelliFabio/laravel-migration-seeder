3f72eae1ff82c09888a264e4678226425b878e6f
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-12-19 16:27:09 +0100

    Changed name of model(Train) & populated the seed

--- database/seeders/TravelTrainsSeeder.php
+++ database/seeders/TravelTrainsSeeder.php
@@ -6,7 +6,7 @@ namespace Database\Seeders;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Trains;
+use App\Models\Train;
 
 class TravelTrainsSeeder extends Seeder
 {
@@ -17,16 +17,17 @@ class TravelTrainsSeeder extends Seeder
      */
     public function run(Faker $faker) {
         for($i = 0; $i < 10; $i++) {
-            $newtrains = new Trains();
-            $newtrains->azienda = $faker->words(2);
-            $newtrains->stazione_partenza = $faker->words(4);
-            $newtrains->stazione_arrivo = $faker->words(4);
+            $newtrains = new Train();
+            $newtrains->azienda = $faker->name();
+            $newtrains->stazione_partenza = $faker->city();
+            $newtrains->stazione_arrivo = $faker->city();
             $newtrains->orario_partenza = $faker->time();
             $newtrains->orario_arrivo = $faker->time();
             $newtrains->codice_treno = $faker->bothify();
             $newtrains->numero_carrozze = $faker->randomDigit();
             $newtrains->in_orario = $faker->boolean();
             $newtrains->cancellato = $faker->boolean();
+            $newtrains->save();
         }
     }
 }
